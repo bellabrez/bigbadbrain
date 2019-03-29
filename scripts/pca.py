b08b5727031267c4c3db6abd88784b4bec207f52
Luke Brezovec <brezovec@stanford.edu>
2019-03-28 18:21:36 -0700

    moved pca file.

--- scripts/pca.py
+++ scripts/pca.py
@@ -8,11 +8,8 @@ from sklearn.decomposition import PCA
 sys.path.insert(0, '/home/users/brezovec/.local/lib/python3.6/site-packages/lib/python/')
 import ants
 
-from BigBadBrain.brain import bleaching_correction, z_score_brain, get_resolution, load_numpy_brain
-from BigBadBrain.fictrac import load_fictrac, prep_fictrac
+from BigBadBrain.brain import bleaching_correction, z_score_brain, get_resolution
 from BigBadBrain.utils import load_timestamps, sort_nicely, send_email
-from BigBadBrain.glm import fit_glm, save_glm_map
-from BigBadBrain.motcorr import get_motcorr_brain
 
 class Fly:
     def __init__(self):
@@ -29,7 +26,7 @@ flies = [Fly() for i in range(len(fly_folders))]
 print('Created flies from folders {}'.format(fly_folders))
 sys.stdout.flush()
 
-desired_flies = [21] # 1 index
+desired_flies = [25] # 1 index
 fly_folders = [fly_folders[i-1] for i in desired_flies]
 flies = [flies[i-1] for i in desired_flies]
 print(fly_folders)
@@ -37,17 +34,48 @@ sys.stdout.flush()
 
 folder = root_path + fly_folders[0]
 
+### Load Brain ###
+
 z_brain_file = os.path.join(folder, 'brain_z.nii')
+try:
+    brain = load_numpy_brain(z_brain_file, channel=None, flip=False)
+except:
+    brain_file = os.path.join(folder, 'motcorr', brain_green.nii)
+    brain = load_numpy_brain(brain_file)
+    brain = bleaching_correction(brain)
+    brain = z_score_brain(brain)
+    save_brain(zbrain_file, brain)
 
-brain = load_numpy_brain(z_brain_file, channel=None, flip=False)
+### Perform PCA ###
 
-#every row is a time point of the whole brain (so flatten brain for each row)
-X = np.reshape(brain, (-1, brain.shape[-1]))
+for swap in [True, False]:
 
-t0 = time()
-pca = PCA()
-PCs = pca.fit_transform(X)
-print(time()-t0)
+    #every row is a time point of the whole brain (so flatten brain for each row)
+    X = np.reshape(brain, (-1, brain.shape[-1]))
 
-save_file = os.path.join(folder, 'pca_out_flipped')
-np.save(save_file,PCs)
+    if swap:
+        X = np.swapaxes(X, 0, 1)
+
+    t0 = time()
+    pca = PCA(n_components=2)
+    #PCs = pca.fit_transform(X)
+    pca.fit(X)
+
+    print(time()-t0)
+
+    ### Save Output ###
+
+    # Make subfolder if it doesn't exist
+    subfolder = 'pca'
+    directory = os.path.join(folder, subfolder)
+    if not os.path.exists(directory):
+        os.makedirs(directory)
+
+    save_file = os.path.join(directory, 'pca_explained_variance_ratio' + str(swap))
+    np.save(save_file,np.asarray(pca.explained_variance_ratio_))
+
+    save_file = os.path.join(directory, 'pca_components' + str(swap))
+    np.save(save_file,np.asarray(pca.components_))
+
+    save_file = os.path.join(directory, 'pca_singular_values' + str(swap))
+    np.save(save_file,np.asarray(pca.singular_values_))
