c15d228de022d4e0829c2608735854fd4f7e7c23
Luke Brezovec <brezovec@stanford.edu>
2019-04-04 10:58:20 -0700

    params.

--- scripts/glm_multiple_behavior_sigma.py
+++ scripts/glm_multiple_behavior_sigma.py
@@ -14,7 +14,7 @@ from BigBadBrain.glm import fit_glm, save_glm_map
 from BigBadBrain.motcorr import get_motcorr_brain
 
 root_path = '/oak/stanford/groups/trc/data/Brezovec/2P_Imaging/20190101_walking_dataset/'
-desired_flies = [27] # 1 index
+desired_flies = [28] # 1 index
 deprecated_motcorr = False
 folders = get_fly_folders(root_path, desired_flies)
 
