58f38672ff3aebaf4380fba3e4e3bf4b16c19aaf
Luke Brezovec <brezovec@stanford.edu>
2019-04-18 09:27:57 -0700

    restructuring scripts.

--- scripts/glm_master_20190412.py
+++ scripts/glm_master_20190412.py
@@ -3,37 +3,31 @@ from time import time
 import os
 import sys
 import scipy
-
 sys.path.insert(0, '/home/users/brezovec/.local/lib/python3.6/site-packages/lib/python/')
 import ants
-
-from BigBadBrain.brain import bleaching_correction, z_score_brain, get_resolution, save_brain, load_numpy_brain, get_dims
-from BigBadBrain.fictrac import load_fictrac, interpolate_fictrac
-from BigBadBrain.utils import load_timestamps, get_fly_folders, send_email, announce_start, timing
-from BigBadBrain.glm import fit_glm, save_glm_map
-from BigBadBrain.motcorr import get_motcorr_brain
+import BigBadBrain as bbb
 
 @timing
 def get_z_brain(directory, channel):
     zbrain_file = os.path.join(directory, 'brain_zscored_' + channel + '.nii')
     try:
         print('Trying to load z-scored brain.')
-        brain = load_numpy_brain(zbrain_file)
+        brain = bbb.load_numpy_brain(zbrain_file)
     except:
         print('Failed. Trying to load motion corrected brain.')
-        brain = get_motcorr_brain(directory, channel=channel)
+        brain = bbb.get_motcorr_brain(directory, channel=channel)
 
         ### Bleaching correction (per voxel) ###
-        brain = bleaching_correction(brain)
+        brain = bbb.bleaching_correction(brain)
 
         ### Z-score brain ###
-        brain = z_score_brain(brain)
-        save_brain(zbrain_file, brain)
+        brain = bbb.z_score_brain(brain)
+        bbb.save_brain(zbrain_file, brain)
     return brain
 
 root_path = '/oak/stanford/groups/trc/data/Brezovec/2P_Imaging/20190101_walking_dataset/'
 desired_flies = [29, 30] # 1 index
-folders = get_fly_folders(root_path, desired_flies)
+folders = bbb.get_fly_folders(root_path, desired_flies)
 
 beta_len = 21 #MUST BE ODD
 fps = 50 #of fictrac camera
@@ -48,21 +42,21 @@ for fly_idx, folder in enumerate(folders):
     for brain_region in brain_regions:
 
         directory = os.path.join(folder, brain_region)
-        announce_start(directory, fly_idx, folders)
-        timestamps = load_timestamps(directory)
-        fictrac = load_fictrac(directory)
+        bbb.announce_start(directory, fly_idx, folders)
+        timestamps = bbb.load_timestamps(directory)
+        fictrac = bbb.load_fictrac(directory)
         
         for channel in channels:
             brain = get_z_brain(directory, channel)
-            dims = get_dims(brain)
+            dims = bbb.get_dims(brain)
             for behavior in behaviors:
                 for sigma in fictrac_sigmas:
 
                     ### Prep given behavior ###
-                    fictrac_interp = interpolate_fictrac(fictrac, timestamps, fps, dur, behavior=behavior, sigma=sigma)
+                    fictrac_interp = bbb.interpolate_fictrac(fictrac, timestamps, fps, dur, behavior=behavior, sigma=sigma)
                 
                     ### Fit GLM ###
-                    scores, betas = fit_glm(brain, fictrac_interp, beta_len)
+                    scores, betas = bbb.fit_glm(brain, fictrac_interp, beta_len)
 
                     ### Save brain ###
-                    save_glm_map(scores, betas, directory, channel, param=behavior)
+                    bbb.save_glm_map(scores, betas, directory, channel, param=behavior)
