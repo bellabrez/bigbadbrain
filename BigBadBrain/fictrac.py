e55d9728e832c2a1c7cb538d59170b04c512a290
Luke Brezovec <brezovec@stanford.edu>
2019-04-23 17:19:43 -0700

    fictrac diff option.

--- BigBadBrain/fictrac.py
+++ BigBadBrain/fictrac.py
@@ -112,7 +112,7 @@ def interpolate_fictrac(fictrac, timestamps, fps, dur, behavior='speed',sigma=3,
     elif sign is not None and sign == 'minus':
       fictrac_smoothed = np.clip(fictrac_smoothed,a_min=None,a_max=0)
     elif sign is not None and sign == 'df':
-      fictrac_smoothed = np.diff(fictrac_smoothed)
+      fictrac_smoothed = np.append(np.diff(fictrac_smoothed),0)
 
     # Interpolate
     # Warning: interp1d set to fill in out of bounds times
