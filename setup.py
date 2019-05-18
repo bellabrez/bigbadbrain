5423c26b722f3457ead3daf4643cb4253739ad8f
Luke Brezovec <brezovec@stanford.edu>
2019-05-18 14:03:01 -0700

    repo name change

--- setup.py
+++ setup.py
@@ -1,10 +1,10 @@
 from setuptools import setup, find_packages
 
 setup(
-    name="BigBadBrain",
+    name="bigbadbrain",
     version="0.0.1",
     long_description=__doc__,
-    packages=['BigBadBrain'],
+    packages=['bigbadbrain'],
     include_package_data=True,
     install_requires=[],
     zip_safe=False
