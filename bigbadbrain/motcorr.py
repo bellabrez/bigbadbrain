afc5e7ed497eed152aa6b331a53434c59356829c
Luke Brezovec <brezovec@stanford.edu>
2019-05-18 15:00:33 -0700

    stopping itk prints

--- bigbadbrain/motcorr.py
+++ bigbadbrain/motcorr.py
@@ -192,6 +192,12 @@ class HiddenPrints:
         self._original_stdout = sys.stdout
         sys.stdout = open(os.devnull, 'w')
 
+        self._original_stderr = sys.stderr
+        sys.stderr = open(os.devnull, 'w')
+
     def __exit__(self, exc_type, exc_val, exc_tb):
         sys.stdout.close()
         sys.stdout = self._original_stdout
+
+        sys.stderr.close()
+        sys.stderr = self._original_stderr
