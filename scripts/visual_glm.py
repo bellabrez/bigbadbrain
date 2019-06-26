99271b89ec4e5b3c4781289e12b3373a3db2f86c
Luke Brezovec <brezovec@stanford.edu>
2019-06-25 22:56:16 -0700

    visual glm

--- scripts/visual_glm.py
+++ scripts/visual_glm.py
@@ -21,20 +21,20 @@ def main(args):
         stimulus = json.load(f)[stim_idx]
     print('Visual glm says stim_idx is {}'.format(stim_idx))
 
-    #brain = bbb.get_z_brain(directory, channel)
-    #dims = bbb.get_dims(brain)
+    brain = bbb.get_z_brain(directory, channel)
+    dims = bbb.get_dims(brain)
 
     ### Fit GLM ###
-    #scores, betas = bbb.fit_visual_glm(brain, stimulus, timestamps, bin_size, pre_dur, post_dur)
+    scores, betas = bbb.fit_visual_glm(brain, stimulus, timestamps, bin_size, pre_dur, post_dur)
 
     ### Save brain ###
     stimulus_no_times = copy.deepcopy(stimulus)
     stimulus_no_times.pop('times', None)
-    #metadict = bbb.make_glm_meta_dict('visual', channel, stimulus_no_times)
+    metadict = bbb.make_glm_meta_dict('visual', channel, stimulus_no_times)
 
     print('Visual glm says stimulus is {}'.format(stimulus_no_times))
 
-    #bbb.save_glm_map(scores, betas, directory, metadict)
+    bbb.save_glm_map(scores, betas, directory, metadict)
 
 if __name__ == '__main__':
     main(sys.argv[1:])
