363087207c7e270c95a97ad1b48129a37e1ad65b
Luke Brezovec <brezovec@stanford.edu>
2019-11-23 19:28:50 -0800

    added anat warp script.

--- scripts/perform_X_on_all_flies.py
+++ scripts/perform_X_on_all_flies.py
@@ -16,12 +16,12 @@ def main():
     root_directory = '/oak/stanford/groups/trc/data/Brezovec/2P_Imaging/20190101_walking_dataset/'
     fly_folders = [os.path.join(root_directory,x) for x in os.listdir(root_directory) if 'fly' in x]
     bbb.sort_nicely(fly_folders)
-    fly_folders = fly_folders[::-1]
+    #fly_folders = fly_folders[::-1]
 
     # TO SELECT ONLY A SUBSET OF FLIES
 
     #fly_folders = [os.path.join(root_directory, 'fly_68'), os.path.join(root_directory, 'fly_69')]
-    #fly_folders = [1]
+    fly_folders = []
     #for i in np.arange(3,5,7,19,21,48,51,54,68):
     for i in [1]:
         fly_folders.append(os.path.join(root_directory, 'fly_{}'.format(i)))
