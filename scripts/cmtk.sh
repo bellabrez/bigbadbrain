975a612e00a8e774b5339dd86be8d6a050d98ac0
Luke Brezovec <brezovec@stanford.edu>
2019-12-02 15:57:18 -0800

    cmtk script

--- scripts/cmtk.sh
+++ scripts/cmtk.sh
@@ -7,10 +7,11 @@
 #SBATCH --output=./output/slurm-%j.out
 
 echo $PWD
-#. /home/users/brezovec/temp
-
+cd /home/users/brezovec/temp
+echo $PWD
 
 #master="/home/users/brezovec/temp/Meanbrain.nii"
 #slave="/home/users/brezovec/temp/Template.nii"
 
-(cd /home/users/brezovec/temp && exec munger -a -w -X 26 -C 8 -G 80 -R 4 -A '--accuracy 0.4' -W 'accuracy 0.4' -T 4 -s Meanbrain.nii Template.nii)
+#(cd /home/users/brezovec/temp && exec munger -a -w -X 26 -C 8 -G 80 -R 4 -A '--accuracy 0.4' -W 'accuracy 0.4' -T 4 -s Meanbrain.nii Template.nii)
+munger -a -w -X 26 -C 8 -G 80 -R 4 -A '--accuracy 0.4' -W 'accuracy 0.4' -T 4 -s Meanbrain.nii Template.nii
