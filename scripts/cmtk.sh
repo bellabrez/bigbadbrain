52940849f1dc11844da41f0b1003a8d49be218e3
Luke Brezovec <brezovec@stanford.edu>
2019-12-04 09:07:19 -0800

    cmtk script

--- scripts/cmtk.sh
+++ scripts/cmtk.sh
@@ -5,13 +5,14 @@
 #SBATCH --ntasks=1
 #SBATCH --cpus-per-task=4
 #SBATCH --output=./output/slurm-%j.out
+#SBATCH --mail-type=ALL
 
 echo $PWD
 cd /home/users/brezovec/temp
 echo $PWD
 
-#master="/home/users/brezovec/temp/Meanbrain.nii"
-#slave="/home/users/brezovec/temp/Template.nii"
+master=Meanbrain.nii
+slave=Template.nii
 
-#(cd /home/users/brezovec/temp && exec munger -a -w -X 26 -C 8 -G 80 -R 4 -A '--accuracy 0.4' -W 'accuracy 0.4' -T 4 -s Meanbrain.nii Template.nii)
-munger -a -w -X 26 -C 8 -G 80 -R 4 -A '--accuracy 0.4' -W '--accuracy 0.4' -T 4 -s Meanbrain.nii Template.nii
+munger -a -w -X 26 -C 8 -G 80 -R 4 -A '--accuracy 0.4' -W '--accuracy 0.4' -T 4 -s $master $slave
+cmtk reformatx -o ./warped/${master}_warped.nii --floating Template.nii Meanbrain.nii Registration/warp/Meanbrain_Template.nii_warp_m0g80c8e1e-1x26r4.list/
