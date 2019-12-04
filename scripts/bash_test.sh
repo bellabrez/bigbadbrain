e54cb834413961f176ceaf14e559d11e7e724c5b
Luke Brezovec <brezovec@stanford.edu>
2019-12-04 08:54:41 -0800

    cmtk script

--- scripts/bash_test.sh
+++ scripts/bash_test.sh
@@ -1,17 +1,25 @@
 #!/bin/bash
-#SBATCH --job-name=cmtk
+#SBATCH --job-name=test
 #SBATCH --partition=trc
-#SBATCH --time=2:00:00
+#SBATCH --time=0:01:00
 #SBATCH --ntasks=1
-#SBATCH --cpus-per-task=4
+#SBATCH --cpus-per-task=1
 #SBATCH --output=./output/slurm-%j.out
 
-echo $PWD
 cd /home/users/brezovec/temp
-echo $PWD
+
+for f in *.nii
+do
+echo "$f"
+done
+
+
+#echo $PWD
+#cd /home/users/brezovec/temp
+#echo $PWD
 
 #master="/home/users/brezovec/temp/Meanbrain.nii"
 #slave="/home/users/brezovec/temp/Template.nii"
 
 #(cd /home/users/brezovec/temp && exec munger -a -w -X 26 -C 8 -G 80 -R 4 -A '--accuracy 0.4' -W 'accuracy 0.4' -T 4 -s Meanbrain.nii Template.nii)
-munger -a -w -X 26 -C 8 -G 80 -R 4 -A '--accuracy 0.4' -W '--accuracy 0.4' -T 4 -s Meanbrain.nii Template.nii
+#munger -a -w -X 26 -C 8 -G 80 -R 4 -A '--accuracy 0.4' -W '--accuracy 0.4' -T 4 -s Meanbrain.nii Template.nii
