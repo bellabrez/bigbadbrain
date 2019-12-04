fbb8ebd9bf56793eeaf72e27bd79889758ddec23
Luke Brezovec <brezovec@stanford.edu>
2019-12-04 10:32:44 -0800

    cmtk script

--- scripts/cmtk.sh
+++ scripts/cmtk.sh
@@ -8,11 +8,16 @@
 #SBATCH --mail-type=ALL
 
 echo $PWD
-cd /home/users/brezovec/temp
+cd /oak/stanford/groups/trc/data/Brezovec/2P_Imaging/20190101_walking_dataset/20191204_anatomy_collection/
 echo $PWD
 
-master=Meanbrain.nii
-slave=Template.nii
+for f in *.nii
+do
+echo "$f"
+done
 
-munger -a -w -X 26 -C 8 -G 80 -R 4 -A '--accuracy 0.4' -W '--accuracy 0.4' -T 4 -s $master $slave
-cmtk reformatx -o ./warped/${master}_warped.nii --floating Template.nii Meanbrain.nii Registration/warp/Meanbrain_Template.nii_warp_m0g80c8e1e-1x26r4.list/
+#master=Meanbrain.nii
+#slave=Template.nii
+
+#munger -a -w -X 26 -C 8 -G 80 -R 4 -A '--accuracy 0.4' -W '--accuracy 0.4' -T 4 -s $master $slave
+#cmtk reformatx -o ./warped/${master}_warped.nii --floating Template.nii Meanbrain.nii Registration/warp/Meanbrain_Template.nii_warp_m0g80c8e1e-1x26r4.list/
