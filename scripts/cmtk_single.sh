f31c39bf444eaa6adbde934ff504d41074ce1109
Luke Brezovec <brezovec@stanford.edu>
2019-12-08 18:44:00 -0800

    syncing

--- scripts/cmtk_single.sh
+++ scripts/cmtk_single.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-#SBATCH --job-name=cmtk_s
+#SBATCH --job-name=cmtk_s_m
 #SBATCH --partition=trc
 #SBATCH --time=2:00:00
 #SBATCH --ntasks=1
@@ -10,11 +10,10 @@ echo $PWD
 cd /oak/stanford/groups/trc/data/Brezovec/2P_Imaging/20190101_walking_dataset/20191204_anatomy_collection/
 echo $PWD
 
-
-
-master=Template.nii
+master=meanbrain20191206.nii
 slave="$1"
+
 echo "slave"
 echo $slave
 munger -a -w -X 26 -C 8 -G 80 -R 4 -A '--accuracy 0.4' -W '--accuracy 0.4' -T 4 -s $master $slave
-cmtk reformatx -o ./warped/${slave::-4}_warped.nii --floating ${slave} ${master} Registration/warp/${master::-4}_${slave}_warp_m0g80c8e1e-1x26r4.list/
+cmtk reformatx -o ./warp2mean2/${slave::-4}_warped.nii --floating ${slave} ${master} Registration/warp/${master::-4}_${slave}_warp_m0g80c8e1e-1x26r4.list/
--- scripts/cmtk_trigger.sh
+++ scripts/cmtk_trigger.sh
@@ -1,14 +1,12 @@
 #!/bin/bash
-#SBATCH --job-name=cmtk_t
+#SBATCH --job-name=cmtk_t_m
 #SBATCH --partition=trc
 #SBATCH --time=1:00:00
 #SBATCH --ntasks=1
 #SBATCH --cpus-per-task=1
 #SBATCH --output=./output/slurm-%j.out
 
-echo $PWD
 cd /oak/stanford/groups/trc/data/Brezovec/2P_Imaging/20190101_walking_dataset/20191204_anatomy_collection/
-echo $PWD
 
 #f=f21a0.nii
 for f in *.nii
@@ -17,11 +15,4 @@ echo "$f"
 cd /home/users/brezovec/projects/bigbadbrain/scripts/
 echo $PWD
 sbatch cmtk_single.sh $f
-#sbatch "/home/users/brezovec/projects/bigbadbrain/scripts/cmtk_single.sh $f"
 done
-
-#master=Meanbrain.nii
-#slave=Template.nii
-
-#munger -a -w -X 26 -C 8 -G 80 -R 4 -A '--accuracy 0.4' -W '--accuracy 0.4' -T 4 -s $master $slave
-#cmtk reformatx -o ./warped/${master}_warped.nii --floating Template.nii Meanbrain.nii Registration/warp/Meanbrain_Template.nii_warp_m0g80c8e1e-1x26r4.list/
