92eccafeee542b6ec96ec38962c2260bc85a4f0b
Luke Brezovec <brezovec@stanford.edu>
2019-12-08 18:54:13 -0800

    warping

--- scripts/cmtk_single.sh
+++ scripts/cmtk_single.sh
@@ -10,10 +10,11 @@ echo $PWD
 cd /oak/stanford/groups/trc/data/Brezovec/2P_Imaging/20190101_walking_dataset/20191204_anatomy_collection/
 echo $PWD
 
-master=meanbrain20191206.nii
-slave="$1"
+master=meanbrain20191208.nii
+#slave="$1"
+slave=kevinmean.nii
 
 echo "slave"
 echo $slave
 munger -a -w -X 26 -C 8 -G 80 -R 4 -A '--accuracy 0.4' -W '--accuracy 0.4' -T 4 -s $master $slave
-cmtk reformatx -o ./warp2mean2/${slave::-4}_warped.nii --floating ${slave} ${master} Registration/warp/${master::-4}_${slave}_warp_m0g80c8e1e-1x26r4.list/
+cmtk reformatx -o ./mean2kevin/${slave::-4}_warped.nii --floating ${slave} ${master} Registration/warp/${master::-4}_${slave}_warp_m0g80c8e1e-1x26r4.list/
